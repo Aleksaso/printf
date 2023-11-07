67f1cbb4d9a9a89a1b4991e3b0279cec31e2b31a
aleks <space42m@gmail.com>
2023-11-07 07:24:02 +0100

    reiniciar

---
+++ Makefile
@@ -0,0 +1,32 @@
+
+NAME = libftprintf.a
+
+SRC = ft_printf.c\
+ft_char.c\
+ft_nbr.c\
+ft_str.c\
+ft_hexd.c\
+ft_simbol.c\
+
+
+OBJS = $(SRC:.c=.o)
+
+FLAGS = -Wall -Wextra -Werror
+
+all : $(NAME) $(OBJS)
+
+$(NAME): $(OBJS)
+	@ar rcs $(NAME) $(OBJS)
+
+clean:
+	@rm -f $(OBJS)
+
+fclean: clean
+	@rm -f $(NAME)
+
+re: fclean all
+
+	@ar rcs $(NAME)
+
+	@gcc $(FLAGS) -c
+.PHONY: all clean fclean re
---
+++ ft_printf.h
@@ -0,0 +1,27 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: asilva-o <asilva-o@student.42malaga.com    +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2023/10/25 22:13:57 by asilva-o          #+#    #+#             */
+/*   Updated: 2023/11/02 20:39:26 by asilva-o         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#ifndef FT_PRINTF.H
+# define FT_PRINTF.H
+# include <unistd.h>
+# include <stdio.h>
+
+typedef struct s_list
+{
+	colocar el prototipo de las funcion
+}
+
+
+
+
+
+#endif
